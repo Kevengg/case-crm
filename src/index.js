64529d951bd4ff0568a3532234406bbba2e18963
Kevengg <kevinandre.engell@gmail.com>
2026-09-07 18:41:43 +0200

    moved default react ap to folder and added DEFAULT env variable as toggle

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './defaults/index.css';
+import DefaultApp from './defaults/App.tsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (process.env.DEFAULT === "true") {
+  root.render(
+    <React.StrictMode>
+      <DefaultApp />
+    </React.StrictMode>
+  );
+} else {
+  root.render(
+    <React.StrictMode>
+      <DefaultApp />
+    </React.StrictMode>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
