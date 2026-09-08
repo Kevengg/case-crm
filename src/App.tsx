c50bbf56ecb032ab97ec1fc0264a3b79c8a39f08
Blondie1705 <iver.midtlyng@gmail.com>
2026-09-08 16:53:40 +0200

    started making customer name card
    Signed-off-by: Blondie1705 <iver.midtlyng@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-// Entrance for custom part of react app
-
 import { FC } from "react";
+import CustomerNameCard from "./Components/CustomerNameCard";
+import { testCostomer } from "./test data/Customers.ts";
 
 const App: FC = () => {
-	return <div>Add custom react here</div>;
+	return <div>
+		<CustomerNameCard {...testCostomer}></CustomerNameCard>
+	</div>;
 };
 
 export default App;
